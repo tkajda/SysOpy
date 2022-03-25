600a82406995f4072e1a24b409a2bcb05c7ca429
tkajda <tomaszkajda4@gmail.com>
2022-03-25 18:41:57 +0100

    zad01 done

--- LAB3/zad01/main.c
+++ LAB3/zad01/main.c
@@ -1,12 +1,37 @@
 //
 // Created by tomasz on 23.03.2022.
 //
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 int main(int argc, char** argv) {
 
-    printf("Hello world");
+    if (argc!=2) exit(1);
+
+    int n = atoi(argv[1]);
+
+
+    for (int i = 0; i < n; i++) {
+        pid_t pid = fork();
+        if(pid==-1) {
+            perror("fork failed");
+            exit(1);
+        }
+
+        if (pid==0) {
+            printf("child: %d, process pid: %d\n",i+1, getpid());
+            return(i+1);
+        }
+        else {
+            int j;
+            pid = wait(&j);
+            printf("Received %d\n", WEXITSTATUS(j));
+        }
+
+    }
     return 0;
 
 }
