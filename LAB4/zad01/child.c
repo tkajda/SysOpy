4cc6099cc5602e9890ae5ce4ec5736ab6b7cd272
tkajda <tomaszkajda4@gmail.com>
2022-04-03 18:17:53 +0200

    zad01 done

--- LAB4/zad01/child.c
+++ LAB4/zad01/child.c
@@ -2,9 +2,29 @@
 // Created by Tomasz Kajda on 03.04.2022.
 //
 
-int main() {
+#define _XOPEN_SOURCE 500
+#include <stdlib.h>
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <time.h>
 
 
+int main(int argc, char**argv) {
+
+    char * status = argv[1];
+
+    if (strcmp(status, "pending") != 0) {
+        raise(SIGUSR1);
+    }
+
+    if (strcmp(status, "mask") == 0 || strcmp(status, "pending") == 0) {
+        sigset_t newmask;
+        sigpending(&newmask);
+        printf("Exec signal pending: %d\n", sigismember(&newmask, SIGUSR1));
+    }
 
 
 
--- LAB4/zad01/main.c
+++ LAB4/zad01/main.c
@@ -12,26 +12,20 @@
 #include <time.h>
 
 void handle_sigusr1(int sig) {
+    usleep(50000);
+    fflush(NULL);
     printf("signal handled in proccess %d\n", getpid());
 }
 
-
 void funcForChild() {
-    while(1) {
-        printf("Hello from PID: %d time: %ld\n", getpid(), clock());
-        usleep(500000);
-    }
-
-
+    printf("Hello from PID: %d\n", getpid());
 }
 
 
 int main(int argc, char** argv) {
 
     printf("%s\n", argv[1]);
-    printf("main PID = %d\n", getpid());
     char * status = argv[1];
-
 
     if (strcmp(status,"ignore")==0) {
         signal(SIGUSR1, SIG_IGN);
@@ -41,37 +35,46 @@ int main(int argc, char** argv) {
         sa.sa_handler = &handle_sigusr1;
         sigaction(SIGUSR1, &sa, NULL);
     }
-    else if (strcmp(status,"mask")==0) {
+
+    else if (strcmp(status,"mask")==0 || strcmp(status,"pending")==0) {
         sigset_t newmask;
         sigemptyset(&newmask);
         sigaddset(&newmask, SIGUSR1);
-        if (sigprocmask(SIG_BLOCK, &newmask, NULL) < 0)
-            perror("cannot block signal\n");
         if (sigprocmask(SIG_BLOCK, &newmask, NULL) < 0) {
-            perror("signal blocked\n");
+            printf("unable to block signal");
+        }
+    }
+
+    printf("main PID = %d\n", getpid());
+
+    raise(SIGUSR1);
+    sigset_t newmask;
+
+    if (strcmp(status, "mask") == 0 || strcmp(status, "pending") == 0) {
+        sigpending(&newmask);
+        printf("Signal pending: %d\n", sigismember(&newmask, SIGUSR1));
+    }
+    if (strcmp(argv[2], "fork")==0){
+
+        pid_t childpid = fork();
+
+        if (childpid==0) {
+            funcForChild();
+        }
+
+        if (strcmp(status, "pending") != 0) {
+            raise(SIGUSR1);
+        }
+
+        if (strcmp(status, "mask") == 0 || strcmp(status, "pending") == 0) {
+            sigset_t childmask;
+            sigpending(&childmask);
+            printf("Child signal pending: %d\n", sigismember(&childmask, SIGUSR1));
         }
 
     }
-    else if (strcmp(status,"pending")==0) {
-    }
     else {
-        exit(1);
+        execl("./child", "./child", status, NULL);
     }
-
-    pid_t childpid = fork();
-
-    if (childpid==0) {
-        funcForChild();
-    }
-
-    sleep(1);
-    kill(childpid, SIGUSR1);
-
-    sleep(2);
-    kill(childpid, SIGKILL);
-
-
-
-
     return 0;
 } 
