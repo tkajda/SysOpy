1bab89b6be220bc9d278017e6f71833c82e8605a
tkajda <tomaszkajda4@gmail.com>
2022-04-03 15:37:01 +0200

    w mainie wszystko

--- LAB4/zad01/main.c
+++ LAB4/zad01/main.c
@@ -9,31 +9,13 @@
 #include <time.h>
 
 void handle_sigusr1(int sig) {
-
+    printf("signal handled\n");
 }
 
 
 void funcForChild(char* status) {
 
-    if (strcmp(status,"ignore")==0) {
-        signal(SIGUSR1, SIG_IGN);
-    }
-    else if (strcmp(status,"handle")==0) {
-        struct sigaction sa;
-        sa.sa_handler = &handle_sigusr1;
-        sigaction(SIGUSR1, &sa, NULL);
 
-    }
-    else if (strcmp(status,"mask")==0) {}
-    else if (strcmp(status,"pending")==0) {}
-    else {
-        exit(0);
-    }
-
-    while(1) {
-        printf("Hello from PID: %d time: %ld\n", getpid(), clock());
-        sleep(1);
-    }
 
 }
 
@@ -44,24 +26,38 @@ int main(int argc, char** argv) {
 
     printf("%s\n", argv[1]);
     pid_t childpid = fork();
+    char * status = argv[1];
+
 
     if (childpid==0) {
-        funcForChild(argv[1]);
+        if (strcmp(status,"ignore")==0) {
+            signal(SIGUSR1, SIG_IGN);
+        }
+        else if (strcmp(status,"handle")==0) {
+            struct sigaction sa;
+            sa.sa_handler = &handle_sigusr1;
+            sigaction(SIGUSR1, &sa, NULL);
+
+        }
+        else if (strcmp(status,"mask")==0) {}
+        else if (strcmp(status,"pending")==0) {}
+
+
+        while(1) {
+            printf("Hello from PID: %d time: %ld\n", getpid(), clock());
+            usleep(500000);
+        }
+
     }
-    else {
-        printf("waiting...");
-        sleep(2);
-        printf("signal sent");
-        kill(childpid, SIGUSR1);
-        printf("waiting...");
 
-        sleep(4);
-        kill(childpid, SIGKILL);
-        sleep(3);
-        printf("waiting...");
+    sleep(1);
+    kill(childpid, SIGUSR1);
+
+    sleep(2);
+    kill(childpid, SIGKILL);
+    sleep(1.5);
 
 
-    }
 
 
     return 0;
