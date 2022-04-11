6736f6c2c516bcd16b7a63a4e2d0a265e325a5c1
tkajda <tomaszkajda4@gmail.com>
2022-04-12 01:21:20 +0200

    file closed

--- LAB5/zad02/main.c
+++ LAB5/zad02/main.c
@@ -64,6 +64,8 @@ int main(int argc, char** argv) {
             printf("%s", line);
             line = calloc(MAX,sizeof(char));
         }
+        pclose(fp);
+
         free(line);
         free(command);
     }
